6384a34c37bc147d843d85d207a25112e5c66a07
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-03-20 18:49:30 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUbLdpXFKQif6QAVGvk5gabFiUWwcswn8",
+    authDomain: "lets-chat-9bc9d.firebaseapp.com",
+    databaseURL: "https://lets-chat-9bc9d-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-9bc9d",
+    storageBucket: "lets-chat-9bc9d.appspot.com",
+    messagingSenderId: "481710097890",
+    appId: "1:481710097890:web:e39246120097f60c3ea5bb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
